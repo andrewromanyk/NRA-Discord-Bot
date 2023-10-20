480086ef1580d577917f8b8ebad607fe49622414
andrewromanyk <andrewrom321@gmail.com>
2023-10-20 18:53:44 +0300

    started making music vote event

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from nextcord.ext import commands
 import datetime
 import random
 import re
+import sqlite3
 from key import KEY
+import python_url
+
 
 BOT_KEY = KEY
 
 intents = nextcord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="l.", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -33,8 +36,7 @@ async def addrole(interaction: nextcord.Interaction,
         description="rgb колір ролі",
         #default=" ".join([str(random.randint(0, 255)) for i in range(3)]),
         required=False
-    )
-    ):
+    )):
 
     if rgb == None:
         #default value would return the same numbers every time, so this part is needed.
@@ -53,8 +55,46 @@ async def addrole(interaction: nextcord.Interaction,
     role = await interaction.guild.create_role(name=role_name, color=nextcord.Color.from_rgb(*rgb_list))
     await user.add_roles(role)
 
+    await interaction.send(f"You created a role '{role.name}' for {user.mention}", ephemeral=True)
+
     print(f"Created role named {role_name} and colored {rgb_list}")
 
+@bot.slash_command(name="offersong", description="запропонувати пісню для рейтингу")
+async def offersong(interaction: nextcord.Interaction,
+    song_link: str = nextcord.SlashOption(
+        name = "link",
+        required=True
+    )):
 
+    print("Does 0st think work pls")
+
+    conn = sqlite3.connect("bot.db")
+    cursor = conn.cursor()
+
+    print("Does 1st think work pls")
+
+    try:
+        cursor.execute("""CREATE TABLE musicLinks (
+                id TEXT,
+                link TEXT,
+                votes INTEGER
+        )""")
+    except: 
+        print("already exists lmao")
+
+    cursor.execute("""SELECT id FROM musicLinks""")
+
+    musicIds = cursor.fetchall()
+    userId = interaction.user.id
+
+    if str(userId) in musicIds: pass
+    else:
+        cursor.execute("""INSERT INTO musicLinks VALUES (?, ?) """, (userId, song_link))
+
+    print("Done something")
+    
+    python_url.getSongAttr(song_link)
+
+    conn.commit()
 
 bot.run(BOT_KEY)
